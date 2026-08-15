6a36b44faf41aac2fac8ec68f9d268ef0ab0e159
Anastasiia Pozdniakova <nastyaapozdnyakova@gmail.com>
2026-08-15 16:13:06 +0300

    added login test for saucedemo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Verify user can login with valid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  await expect(page.locator('.inventory_list')).toBeVisible();
 });
