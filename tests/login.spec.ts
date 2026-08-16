5f95dbbe724c0cde4240ab20722b21d82ba660b3
Anastasiia Pozdniakova <nastyaapozdnyakova@gmail.com>
2026-08-16 13:18:29 +0300

    renamed login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('User cannot login with wrong password', async ({ page }) => {
+test('Verify user can login with valid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com');
-
-  await page.locator('[data-test="username"]').fill('standard_user')
-  await page.locator('[data-test="password"]').fill('wrong_password');
+  
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-
-  await expect(page.locator('[data-test="error"]')).toBeVisible();
-  });
+  
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  await expect(page.locator('.inventory_list')).toBeVisible();
+});
